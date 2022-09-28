7941d152de96ff58c092de1a47845edde0ff5202
Yuri <12345@mail.ru>
2022-09-29 01:23:10 +0400

    task3_8

--- task3_8/Program.cs
+++ task3_8/Program.cs
@@ -4,29 +4,29 @@
 int[,] array = new int[3, 3];
 FillArray(array);
 PrintIndex(array);
-void PrintIndex(int[,] arr)
+
+void PrintIndex(int[,] array)
 {
-    for (int i = 0; i < array.GetLength(0); i++)
-    {
-        for (int j = 0; j < array.GetLength(1); j++)
-        {
-				Console.Write($"{array[i, j]} ");
-		  }
-			         
-		  Console.WriteLine();
-    }
-}	 
+	for (int i = 0; i < array.GetLength(0); i++)
+	{
+		for (int j = 0; j < array.GetLength(1); j++)
+		{
+			Console.Write($"{array[i, j]} ");
+		}
+		Console.WriteLine();
+	}
+}
 
 void FillArray(int[,] arr)
 {
-    int count = 10;
-    for (int i = 0; i < arr.GetLength(0); i++)
-    {
-        for (int j = 0; j < arr.GetLength(1); j++)
-			{
-				arr[i, j] = new Random().Next(1, 10);
-				arr[i, j] += count;
-         	count += 1;
-			}
-    }
-}   
+	int count = 10;
+	for (int i = 0; i < arr.GetLength(0); i++)
+	{
+		for (int j = 0; j < arr.GetLength(1); j++)
+		{
+			arr[i, j] = new Random().Next(1, 10);
+			arr[i, j] += count;
+			count += 1;
+		}
+	}
+}
