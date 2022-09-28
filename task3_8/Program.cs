af6888d3ccc6734c30f77ecad6a29dd4c77dfb68
Yuri <12345@mail.ru>
2022-09-29 00:47:12 +0400

    task3_8

---
+++ task3_8/Program.cs
@@ -0,0 +1,32 @@
+﻿//Сформируйте двухмерный массив из неповторяющихся двузначных чисел (размер массива не более 50 элементов). 
+//Напишите программу, которая будет построчно выводить массив.
+
+int[,] array = new int[3, 3];
+FillArray(array);
+PrintIndex(array);
+void PrintIndex(int[,] arr)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+				Console.Write($"{array[i, j]} ");
+		  }
+			         
+		  Console.WriteLine();
+    }
+}	 
+
+void FillArray(int[,] arr)
+{
+    int count = 10;
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+			{
+				arr[i, j] = new Random().Next(1, 10);
+				arr[i, j] += count;
+         	count += 1;
+			}
+    }
+}   
